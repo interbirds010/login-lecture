0545771e88d9724cba3431773fe37d422f92b5fc
interbirds010 <interbirds010@gmail.com>
2024-04-30 11:42:27 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
@@ -11,29 +12,11 @@ const output = {
     }
 }
 
-const users = {
-    id: ["woorimIT","나개발","김팀장"],
-    psword: ["1234","1234","1234"]
-}
-
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
-
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-                return res.json({
-                    success: true,
-                });
-            }
-        }
-
-        return res.json({
-            success: false,
-            msg: "로그인에 실패하셨습니다."
-        });
+        const user = new User(req.body);
+        const response = user.login();
+        return res.json(response);
     },
 }
 
