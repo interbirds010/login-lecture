b65a97d1eea029cc9abb76c7eeab624643cc10ff
interbirds010 <interbirds010@gmail.com>
2024-05-01 11:08:27 +0900

    로그분석 winstom

--- app/app.js
+++ app/app.js
@@ -21,6 +21,7 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함도리 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
+// 로그관리 (morgan 외에 winston도 많이 사용함)
 app.use(morgan("common", { stream: accessLogStream }));
 // app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 app.use('/', home);
---
+++ app/src/config/logger.js
@@ -0,0 +1,50 @@
+const { createLogger, transports, format } = require("winston");
+const { combine, timestamp, label, printf, simple, colorize } = format;
+
+// 노출 형식 설정
+const printFormat = printf(({ timestamp, label, level, message }) => {
+    return `${timestamp} [${label}] ${level} : ${message}`;
+});
+
+const printLogFormat = {
+    file: combine(
+        label({
+            label: "백엔드 맛보기",
+        }),
+        timestamp({
+            format: "YYY-MM-DD HH:mm:dd",
+        }),
+        // json(),
+        printFormat
+    ),
+    console: combine(
+        // createLogger에서 파일로 저장할 때는 colorize를 사용하지 않아야 이상한 문자가 저장되지 않음.
+        colorize(),
+        simple()
+    ),
+};
+
+const opts = {
+    file: new transports.File({
+        filename: "access.log",
+        dirname: "./logs",
+        level: "info",
+        format: printLogFormat.file,
+    }),
+    console: new transports.Console({
+        level: "info",
+        format: printLogFormat.console,
+    }),
+}
+
+const logger = createLogger({
+    // 파일로 저장
+    transports: [opts.file],
+});
+
+// 개발 중일때에만 콘솔 노출
+if (process.env.NODE_ENV !== "production") {
+    logger.add(opts.console);
+}
+
+module.exports = logger;
